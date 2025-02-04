063492fb774eb662a45c88235da092732ab315fd
roberto.tuda <roberto.tuda@encora.com>
2025-02-04 15:42:12 -0600

    Permutations2 fixed solution presentation by adding a case when subarray is just an integer

--- Permutations/Permutations1.py
+++ Permutations/Permutations1.py
@@ -1,5 +1,4 @@
 def per(numss,n):
-
     p=[]
     a=[]
     if len(numss)>1:
@@ -7,23 +6,23 @@ def per(numss,n):
             if i<len(numss):
                 a=numss.copy()
                 a.pop(i)
+                a2=[]
                 a2=per(a,n)
-                print(len(a2))
                 for j in range(len(a2)):
                     q=[numss[i]]
-                    q.extend([a2[j]])
-                    print(q)
+                    if isinstance(a2[j], int):
+                        q.extend([a2[j]])
+                    else:
+                        q.extend(a2[j])
                     p.append(q)
-
-
-
     else:
         p.append(numss[0])
-
     return p
 class Solution:
 
     def permute(self, nums: List[int]) -> List[List[int]]:
-        P=per(nums,len(nums))
-
+        if(len(nums)==1):
+            P=[nums.copy()]
+        else:
+            P=per(nums,len(nums))
         return P
